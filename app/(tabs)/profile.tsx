ba3bce84f7cbdb0e627a6357c2f441f2420808c4
LeoTkach <leonidt1908@gmail.com>
2025-05-29 22:02:05 +0300

    fix(tabs): update profile and layout

--- app/(tabs)/profile.tsx
+++ app/(tabs)/profile.tsx
@@ -1,7 +1,25 @@
 import { View } from "react-native";
+import { BigIcon } from "@/core/components/ui/bigIcon";
+import { Accent } from "@/core/components/ui/typography";
+import { Default } from "@/core/components/ui/typography";
+import React from "react";
 
 export default function ProfilePage() {
   return (
-    <View></View>
+    <React.Fragment>
+      <View className="flex-1 justify-between pt-8 pb-10 px-8">
+        <View  className="items-center gap-y-6">
+          <View className="items-center gap-y-2">
+            <BigIcon>
+            </BigIcon>
+            <View className="items-center">
+              <Accent className="text-foreground">John Doe</Accent>
+              <Default className="text-foreground text-center max-w-[220px]">Hello its me, I very like nurvana songs</Default>
+            </View>
+          </View>
+          <Accent className="text-foreground">Social media</Accent>
+        </View>
+      </View>
+    </React.Fragment>
   )
 }
